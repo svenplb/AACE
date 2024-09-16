380a637111b781e5c8cc589174272b4d3e9b298e
sven <97550617+svenplb@users.noreply.github.com>
2024-09-16 12:52:48 +0200

    dumb search algorithm

--- README.md
+++ README.md
@@ -1,17 +1,11 @@
-# YARBCS
+<p align="center"><img src="https://images.emojiterra.com/google/noto-emoji/unicode-15.1/color/1024px/1f9cc.png" height="64" alt="Project Logo"></p>
+<h3 align="center">AACB</h3>
+<p align="center">Anti Adrian Chess Bot, written in Go. Goal to reach 900 ELO to beat my friend Adrian.</p>
+
+## Todo List
 - [x] Chessboard representation
 - [x] random moves
-- [] pieces wert geben, Dame = 9, Turm = 5, Läufer = 3, Pferd = 3, Bauer = 1
-- [] board evaluation
-- [] move generation mit minimax search tree
-- [] alpha-beta pruning (optional)(optmisierung)
-
-
-
-
-
-Notes
-- library: https://github.com/notnil/chess
-
-
-
+- [ ] pieces wert geben, Dame = 9, Turm = 5, Läufer = 3, Pferd = 3, Bauer = 1
+- [ ] board evaluation
+- [ ] move generation mit minimax search tree
+- [ ] alpha-beta pruning (optional)(optmisierung)
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 
 	"github.com/notnil/chess"
 )
@@ -34,6 +33,33 @@ func evaluateBoardPosition(position *chess.Position) int {
 	return score
 }
 
+// func searchTree(position *chess.Position, depth int, alpha int, beta int) int {}
+func searchTree(position *chess.Position, depth int) (int, *chess.Move) {
+	// depth = how deep the alg / tree goes
+	// alpha = best score for maximizing
+	// beta = best score for  minimizing
+	moves := position.ValidMoves()
+
+	// return current state if depth == 0
+	if depth == 0 {
+		return evaluateBoardPosition(position), nil
+	}
+
+	bestEvaluation := -9999
+	var bestMove *chess.Move
+	for _, move := range moves {
+		newPosition := position.Update(move)
+		evaluation, _ := searchTree(newPosition, depth-1)
+		if evaluation > bestEvaluation {
+			bestEvaluation = evaluation
+			bestMove = move
+		}
+
+	}
+	return bestEvaluation, bestMove
+
+}
+
 func main() {
 	game := chess.NewGame()
 	// print outcome and game PGN
@@ -47,15 +73,17 @@ func main() {
 		if len(moves) == 0 {
 			break
 		}
+		bestEvaluation, bestMove := searchTree(game.Position(), 3)
+		fmt.Println("Best Move: " + bestMove.String())
+		fmt.Println("Best Evaluation: ", bestEvaluation)
+		err := game.Move(bestMove)
 
-		randomMove := moves[rand.Intn(len(moves))]
-
-		err := game.Move(randomMove)
 		if err != nil {
 			fmt.Println("Error making move:", err)
 			break
 		}
-		score := (evaluateBoardPosition(game.Position()))
+
+		score := evaluateBoardPosition(game.Position())
 		if score > 0 {
 			fmt.Println("White is winning")
 			fmt.Println(score)
@@ -66,6 +94,8 @@ func main() {
 			fmt.Println("The game is drawn")
 			fmt.Println(score)
 		}
+
+		fmt.Println("Game outcome:", game.Outcome())
 		fmt.Println(game.Position().Board().Draw())
 		fmt.Println(moves)
 
